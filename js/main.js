ae393e0d3fc1acfd82ce2a773aa6ad2fa185ff51
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-04-09 14:53:34 +0000

    finish uploading all files needed

--- js/main.js
+++ js/main.js
@@ -32,4 +32,42 @@ function updateStates() {
     document.querySelector('#head-count').textContent = `Heads: ${heads}`;
     document.querySelector('#tail-count').textContent = `Tails: ${tails}`;
 }
+
+function disablebutton() {
+    flipCoin.disabled = true;
+    setTimeout(function() {
+       flipCoin.disabled = false; 
+    }, 3000);
+}
+
+reset.addEventListener('click',() =>{
+    coin.style.animation = 'none';
+    heads = 0;
+    tails = 0;
+    updateStates();
+})
+
+
+
+
+
+
+// ------------------------------------- X-sign js ---------------------------------------//
     
+
+const signs = document.querySelectorAll('h1')
+const randomIn = (min, max) => (
+  Math.floor(Math.random() * (max - min + 1) + min)
+)
+
+const mixupInterval = el => {
+  const ms = randomIn(2000, 4000)
+  el.style.setProperty('--interval', `${ms}ms`)
+}
+
+signs.forEach(el => {
+  mixupInterval(el)
+  el.addEventListener('webkitAnimationIteration', () => {
+    mixupInterval(el)
+  })
+})
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const fs = require('fs');
+const figlet = require('figlet');
 
+const app = express();
+const PORT = 3000;
+
+// Serve HTML file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(PORT, ()=>{
-    console.log(`this sever is running on port${PORT}!`)
-})
+// Serve static files (images)
+app.use('/images', express.static(__dirname + '/images'));
+
+//  Serve Css files
+app.use('/css', express.static(__dirname + '/css'));
+//  Serve JS files 
+app.use('/js', express.static(__dirname + '/js'));
+// Handle 404 errors
+app.use((req, res) => {
+    figlet('404!!', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        res.send(data);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}!`);
+});
